ee8256af039c59b35961ae57167fccbe1ec61f6e
Сергей <itkse89@cmail.com>
2023-05-20 18:22:33 +0300

    делает блок тренажерный зал

---
+++ source/js/modules/video.js
@@ -0,0 +1,14 @@
+
+const videoBtn = document.getElementById('desc-btn-video');
+const videoContainer = document.getElementById('desc-video');
+const videoWrapper = document.querySelector('.main__desc-video-wrapper');
+
+videoWrapper.classList.remove('main__desc-video-wrapper--no-js');
+
+if (videoBtn) {
+  videoBtn.addEventListener('click', function () {
+    videoContainer.classList.toggle('main__desc-video-wrapper--active');
+  });
+}
+
+
