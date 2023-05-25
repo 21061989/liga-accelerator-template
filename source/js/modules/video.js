ab96ae5c84abdccba4799d8204e9ce11f16f4cc8
Сергей <itkse89@cmail.com>
2023-05-25 19:06:20 +0300

    исправляет недочеты

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -3,7 +3,9 @@ const videoBtn = document.getElementById('desc-btn-video');
 const videoContainer = document.getElementById('desc-video');
 const videoWrapper = document.querySelector('.main__desc-video-wrapper');
 
-videoWrapper.classList.remove('main__desc-video-wrapper--no-js');
+if (videoWrapper) {
+  videoWrapper.classList.remove('main__desc-video-wrapper--no-js');
+}
 
 if (videoBtn) {
   videoBtn.addEventListener('click', function () {
